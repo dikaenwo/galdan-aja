903e8c2c9e78b41361878994736fa1482b9f7287
arninoviani <arniinoviianii@gmail.com>
2025-04-13 21:59:58 +0800

    splash screen, onboarding 1-4, home, detail produk, cart, search active, filter, tambah produk, home penjual

---
+++ app/src/main/java/com/example/galdanaja/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.galdanaja
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    // Durasi splash dalam milidetik (misal 3 detik)
+    private val splashDelay: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashDelay)
+    }
+}
