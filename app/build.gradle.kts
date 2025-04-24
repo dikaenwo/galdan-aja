10da49c842bae96a5084db02c3fa057dd0b39644
dikaenwo <rimaputri9879@gmail.com>
2025-04-24 19:41:48 +0800

    auth google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+
 }
