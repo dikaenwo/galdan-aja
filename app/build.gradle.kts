5d55352514228537f42e5b092bde3d6cb0ba8377
dikaenwo <rimaputri9879@gmail.com>
2025-04-24 13:51:34 +0800

    splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("com.google.firebase:firebase-auth:23.2.0")
+    implementation("androidx.activity:activity:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
--- app/src/main/java/com/example/galdanaja/SplashScreenActivity.kt
+++ app/src/main/java/com/example/galdanaja/SplashScreenActivity.kt
@@ -1,20 +1,29 @@
 package com.example.galdanaja
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.galdanaja.databinding.ActivitySplashScreenBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        lifecycleScope.launch {
+            delay(3000)
+            startActivity(Intent(this@SplashScreenActivity, OnBoardingActivity::class.java))
+            finish()
         }
     }
 }
